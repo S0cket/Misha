d84fe34c5643ecbdcf16af30b9f29fa98417d1a1
S0cket <rus.swordsman@gmail.com>
2020-05-27 14:10:56 +0300

    fix 20

--- 20/20.cpp
+++ 20/20.cpp
@@ -7,53 +7,41 @@ using namespace std;
 int main(void) {
 	srand(time(NULL));
 
-	int n, s = 0;
+	int n;
 	cin >> n;
 	int *a = new int[n];
 	cout << "old:\n";
 	for (int i = 0; i < n; ++ i) {
 		*(a + i) = rand() % 100 - 20;
-		s += *(a + i);
 		cout << *(a + i) << " ";
 	}
 
-	int mv = s / n;
-	cout << "\nsrznach = " << mv;
+	int mv = 0;
 
 	int cnt = 0, ma = -1000, mi = 1000;
 	for (int i = 0; i < n; ++ i) {
 		if (*(a + i) > 0) {
 			++ cnt;
+			mv += *(a + i);
 			if (*(a + i) > ma)
 				ma = *(a + i);
 			if (*(a + i) < mi)
 				mi = *(a + i);
 		}
-		else {
-			if (cnt > 1) { //cnt > 0 => если последовательность из одного элемента тоже считается за последовательность
-				for (int j = i - 1; j >= 0 && *(a + j) > 0; -- j) {
+		if (*(a + i) <= 0 || i == n - 1){
+			if (cnt > 1) {
+				mv /= cnt;
+				for (int j = (*(a + i) > 0) ? i : (i - 1); j >= 0 && *(a + j) > 0; -- j) {
 					if (*(a + j) == ma || *(a + j) == mi)
 						*(a + j) = mv;
 				}
 			}
+			mv = 0;
 			cnt = 0;
 			ma = -1000;
 			mi = 1000;
 		}
 	}
-
-
-	if (cnt > 1) { //cnt > 0 => если последовательность из одного элемента тоже считается за последовательность
-		for (int j = n - 1; j >= 0 && *(a + j) > 0; -- j) {
-			if (*(a + j) == ma || *(a + j) == mi) {
-				*(a + j) = mv;
-			}
-		}
-	}
-	cnt = 0;
-	ma = -1000;
-	mi = 1000;
-
 
 	cout << "\nnew:\n";
 	for (int i = 0; i < n; ++ i)
